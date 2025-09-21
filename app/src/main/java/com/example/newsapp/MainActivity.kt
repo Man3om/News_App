64565d01285e43ea0736def798ff2e5bec42d9c5
a.magdi <menem.magdi222@gmail.com>
2025-09-21 14:34:36 +0300

    - Implement Retrofit and APIs.

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.newsapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,19 +10,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat.enableEdgeToEdge
 import com.example.newsapp.api.ApiManager
+import com.example.newsapp.api.model.SourcesResponse
 import com.example.newsapp.ui.theme.NewsAppTheme
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : ComponentActivity() {
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         setContent {
             NewsAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(),
+                LaunchedEffect(Unit) {
+                    getSources()
+                }
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
                     bottomBar = {
 
                     },
@@ -31,5 +44,26 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    fun getSources() {
+        ApiManager.webServices().getNewsSources().enqueue(object : Callback<SourcesResponse> {
+            override fun onResponse(
+                call: Call<SourcesResponse?>,
+                response: Response<SourcesResponse?>
+            ) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    Log.i(TAG, "onResponse: $responseBody")
+                } else {
+                    val errorBody = response.errorBody()
+                    Log.e(TAG, "onResponse: $errorBody")
+                }
+            }
+
+            override fun onFailure(call: Call<SourcesResponse>, t: Throwable) {
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
     }
 }
--- app/src/main/java/com/example/newsapp/api/WebServices.kt
+++ app/src/main/java/com/example/newsapp/api/WebServices.kt
@@ -4,9 +4,12 @@ import com.example.newsapp.api.model.SourcesResponse
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface WebServices {
 
     @GET("v2/top-headlines/sources")
-    fun getNewsSources() : Call<SourcesResponse>
+    fun getNewsSources(
+        @Query("apiKey") apiKey: String = "06d492c0e0074c2ba430339bf1839078"
+    ): Call<SourcesResponse>
 }

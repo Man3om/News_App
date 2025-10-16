e0b35a9cfe1dee6039c683599da3fa7f8dd902ea
a.magdi <abdelmonemshinaishin@gmail.com>
2025-10-16 12:44:12 +0300

    - Adding Repos to handle Communication between ViewModel and Apis

--- app/src/main/java/com/example/newsapp/ui/screens/News/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/screens/News/NewsViewModel.kt
@@ -2,13 +2,9 @@ package com.example.newsapp.ui.screens.News
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.newsapp.api.ApiManager
 import com.example.newsapp.api.model.everythingResponseApiModel.ArticlesItem
-import com.example.newsapp.api.model.everythingResponseApiModel.EverythingResponse
 import com.example.newsapp.api.model.sourceResponseApiModel.SourcesItemDM
-import com.example.newsapp.api.model.sourceResponseApiModel.SourcesResponse
 import com.example.newsapp.ui.repository.NewsRepo
-import com.google.gson.Gson
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
@@ -17,13 +13,13 @@ class NewsViewModel : ViewModel() {
     val sourcesResource = MutableStateFlow<Resources<List<SourcesItemDM>>>(Resources.Initial())
     val articlesResource = MutableStateFlow<Resources<List<ArticlesItem>>>(Resources.Initial())
 
-    val repositry = NewsRepo()
+    val repository = NewsRepo()
 
 
     fun getSources(categoryApiId: String) {
         viewModelScope.launch {
             sourcesResource.value = Resources.Loading()
-            sourcesResource.value = repositry.getSources(categoryApiId)
+            sourcesResource.value = repository.getSources(categoryApiId)
         }
     }
 
@@ -31,7 +27,7 @@ class NewsViewModel : ViewModel() {
         selectedSourceId.value = sourceId
         viewModelScope.launch {
             articlesResource.value = Resources.Loading()
-            articlesResource.value = repositry.getNewsBySourceId(sourceId)
+            articlesResource.value = repository.getNewsBySourceId(sourceId)
         }
     }
 
